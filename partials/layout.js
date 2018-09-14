f689b42f83e105a378b02be203e48c5487386f0d
Leonardo Matos <leomp120894@gmail.com>
2018-09-14 19:21:17 -0300

    update layout: nav and header

--- partials/layout.js
+++ partials/layout.js
@@ -12,75 +12,93 @@
   'use strict'
 
   var layout = function () {
-    var i, html
+    // var i, html
 
     /* setup layout components */
 
     // API, resource or action title
-    var $title = $('<span>', {
-      'class': 'navbar-text',
+    var $title = $('<h5>', {
+      'class': 'm-0 restform-title',
       text: 'API Console'
     })
 
-    // setup top nav HTML
-    var navs = [ 'Headers', 'Body', 'Schema', 'Form' ]
-    html = []
-    for (i = 0; i < navs.length; i++) {
-      var nav = navs[i]
-      html.push($('<li>', {
-        'class': 'nav-item',
-        id: 'restform-nav-' + nav,
-        html: $('<a>', {
-          'class': 'nav-link',
-          text: nav,
-          href: 'javascript:;',
-          click: (function (nav) {
-            // local nav
-            return function () {
-              // show tab
-            }
-          }(nav))
-        })
-      }))
-    }
-
-    // top navbar menu
-    var $navbar = $('<ul>', {
-      'class': 'navbar-nav ml-auto',
-      html: html
+    // API endpoint elements
+    var $spanMethod = $('<span>', {
+      'class': 'input-group-text',
+      type: 'text',
+      text: 'GET'
     })
+    var $inputUrl = $('<input>', {
+      'class': 'form-control',
+      type: 'text',
+      readonly: true,
+      value: 'https://api.e-com.plus',
+      click: function () {
+        // select all text
+        $(this).select()
+      }
+    })
+    var $buttonSend = $('<button>', {
+      'class': 'btn btn-primary mx-2',
+      type: 'button',
+      'aria-label': 'Send',
+      html: '<i class="ti ti-check mr-1"></i> Send'
+    })
+
+    // sticky nav bar
     var $nav = $('<nav>', {
-      'class': 'navbar navbar-expand-lg navbar-light restform-nav',
+      'class': 'sticky-top shadow-sm restform-nav',
       html: $('<div>', {
         'class': 'container',
         html: [
-          $('<button>', {
-            'class': 'close mr-3',
-            type: 'button',
-            'aria-label': 'Close',
-            html: '<span aria-hidden="true">&times;</span>',
-            click: function () {
-              // hide API console main element
-              $layout.fadeOut()
-            }
-          }),
-          $title,
-          $('<button>', {
-            'class': 'navbar-toggler',
-            type: 'button',
-            'data-toggle': 'collapse',
-            'data-target': '#restform-navbar',
-            'aria-expanded': false,
-            'aria-controls': 'restform-navbar',
-            'aria-label': 'Toggle navigation',
-            html: '<span class="navbar-toggler-icon"></span>'
-          }),
           $('<div>', {
-            'class': 'collapse navbar-collapse',
-            id: 'restform-navbar',
-            html: $navbar
+            'class': 'row no-gutters',
+            html: [
+              $('<div>', {
+                'class': 'col',
+                html: $('<div>', {
+                  'class': 'input-group',
+                  html: [
+                    $('<div>', {
+                      'class': 'input-group-prepend',
+                      html: $spanMethod
+                    }),
+                    $inputUrl,
+                    $('<div>', {
+                      'class': 'input-group-append',
+                      html: '<button class="btn btn-outline-secondary" type="button">Params</button>'
+                    })
+                  ]
+                })
+              }),
+              $('<div>', {
+                'class': 'col-auto',
+                html: [
+                  $buttonSend,
+                  $('<button>', {
+                    'class': 'btn btn-light',
+                    type: 'button',
+                    'aria-label': 'Close',
+                    html: '<i class="ti ti-close"></i>',
+                    click: function () {
+                      // hide API console main element
+                      $layout.fadeOut()
+                    }
+                  })
+                ]
+              })
+            ]
           })
         ]
+      })
+    })
+
+    // title header
+    var $header = $('<header>', {
+      'class': 'restform-header',
+      html: $('<div>', {
+        'class': 'container',
+        html: $title
       })
     })
 
@@ -88,14 +106,17 @@
     var $layout = $('<article>', {
       'class': 'restform',
       html: [
-        $nav
+        $nav,
+        $header
       ]
     })
 
     // return object with DOM elements
     return {
       $title: $title,
-      $nav: $nav,
+      $spanMethod: $spanMethod,
+      $inputUrl: $inputUrl,
+      $buttonSend: $buttonSend,
       $layout: $layout
     }
   }
